bcf4f1da811d47c84614c72c545bba0a19206734
minchina <minchina91@gmail.com>
2017-05-15 17:14:44 +0800

    update config server and client

---
+++ configclient/src/main/java/org/ncmao/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package org.ncmao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	private String foo;
+
+	@RequestMapping(value = "/hi", method = RequestMethod.GET)
+	public String hi(){
+		return foo;
+	}
+}
